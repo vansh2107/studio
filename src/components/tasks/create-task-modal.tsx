a4254dc8ded3d3e4e71826f0ac2f3bb56ec59ec8
RUSH GAMERZ <gandhivansh479@gmail.com>
2025-12-31 09:05:50 +0000

    Try fixing this error: `Runtime ReferenceError: useEffect is not defined

--- src/components/tasks/create-task-modal.tsx
+++ src/components/tasks/create-task-modal.tsx
@@ -1,6 +1,7 @@
+
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
