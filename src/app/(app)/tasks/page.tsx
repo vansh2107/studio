378b28388ba92afe77ce94e850fe7937a8312e84
RUSH GAMERZ <gandhivansh479@gmail.com>
2026-01-07 12:16:53 +0000

    Try fixing this error: `Runtime ReferenceError: React is not defined. Er

--- src/app/(app)/tasks/page.tsx
+++ src/app/(app)/tasks/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import {
   Table,
