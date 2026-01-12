d3943bf024e4d18d5dc6796bd4e4bdc4edac3aae
RUSH GAMERZ <gandhivansh479@gmail.com>
2026-01-12 17:46:02 +0000

    ## Error Type Console Error
    ## Error Message
    A tree hydrated but some a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,14 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('font-body antialiased min-h-screen bg-background')}>
+      <body className={cn('font-body antialiased min-h-screen bg-background')} suppressHydrationWarning>
         <UserProvider>
           {children}
         </UserProvider>
